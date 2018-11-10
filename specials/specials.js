bc067f3cf319a4a3f7a4ec2849307b9e59efa7c0
Eesha Sabherwal <esabherwal@wustl.edu>
2018-11-09 18:56:44 -0600

    working on changing nav bar

--- specials/specials.js
+++ specials/specials.js
@@ -213,6 +213,8 @@ var array = ["empty_string"];
         listItem3.className = 'collapse';
         listItem3.id = "sta" + staStr;
         locationdiv.appendChild(listItem3);
+        var br = document.createElement('br');
+        locationdiv.appendChild(br);
       }
 });
 }
