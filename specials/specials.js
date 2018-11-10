c0cfe7df1103c77c39d6da3d0e8df80690579449
Eesha Sabherwal <esabherwal@wustl.edu>
2018-11-09 19:44:57 -0600

    fixing line break for stations

--- specials/specials.js
+++ specials/specials.js
@@ -200,8 +200,8 @@ var array = ["empty_string"];
         }
         if (!(array.contains(sta))) {
             //appends stations
-            var listItem = document.createElement('button');
-            listItem.innerHTML = '<a data-toggle="collapse" href="#sta' + staStr + '"role="button" aria-expanded="false" aria-controls="sta' + staStr + '">' + sta + "</a></br></br>";
+            var listItem = document.createElement('li');
+            listItem.innerHTML = '<a data-toggle="collapse" href="#sta' + staStr + '"role="button" aria-expanded="false" aria-controls="sta' + staStr + '">' + sta + "</a></br>";
             listItem.className = 'btn btn-secondary btn-sm text-center collapse';
             listItem.id = "loc" + locStr;
             locationdiv.appendChild(listItem);
